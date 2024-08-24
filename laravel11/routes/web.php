5b4fd1d9afcbdf2d97d2d673cdd4b11563333e9d
sa0ChunLuyu <sa0Chun_luyu@aliyun.com>
2024-08-24 23:54:48 +0800

    更新 调整 git

--- laravel11/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::any('api/yo', [\App\Http\Controllers\YoController::class, 'test']);
-Route::get('/', function () {
-    return view('welcome');
-});
